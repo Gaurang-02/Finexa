366ca57de8f8249381406c7e08b5e9dd75c3cbca
Gaurang_02 <heygaurang.02@gmail.com>
2025-05-03 20:24:31 +0530

    initialise the folder and structure files and home page

---
+++ app/(root)/page.tsx
@@ -0,0 +1,29 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+import React from 'react'
+
+const Home = () => {
+    const loggedIn = { firstName: 'Gaurang'}
+  return (
+    <section className='no-scrollbar flex w-full flex-row max-xl:max-h-screen max-xl:overflow-y-scroll'>
+        <div className='no-scrollbar flex w-full flex-1 flex-col gap-8 px-5 sm:px-8 py-7 lg:py-12 xl:max-h-screen xl:overflow-y-scroll'>
+            <header className='flex flex-col justify-between gap-8'>
+                <HeaderBox
+                    type="greeting"
+                    title="Welcome"
+                    user={loggedIn?.firstName || 'Guest'}
+                    subtext='Access and Manage your account and Transaction Efficiently'
+                />
+
+                <TotalBalanceBox
+                    accounts={[]}
+                    totalBanks={1}
+                    totalCurrentBalance={1234.5}
+                />
+            </header>
+        </div>
+    </section>
+  )
+}
+
+export default Home
---
+++ components/TotalBalanceBox.tsx
@@ -0,0 +1,32 @@
+import AnimatedCounter from "./AnimatedCounter";
+
+const TotalBalanceBox = ({
+  accounts = [],
+  totalBanks,
+  totalCurrentBalance,
+}: TotlaBalanceBoxProps) => {
+  return (
+    <section className="flex w-full items-center gap-4 rounded-xl border border-gray-200 p-4 shadow-chart sm:gap-6 sm:p-6">
+      <div className="flex size-full max-w-[100px] items-center sm:max-w-[120px]">
+        {/*  */}
+      </div>
+      <div className="flex flex-col gap-6">
+        <h2 className="text-18 font-semibold text-gray-900">
+          Bank Accounts: {totalBanks}
+        </h2>
+        <div className="flex-flex-col gap-2">
+            <p className="text-14 font-medium text-gray-600">
+                Total Current Balance
+            </p>
+
+            <p className="text-24 lg:text-30 flex-1 font-semibold text-gray-900 gap-2 flex-center">
+                <AnimatedCounter amount={totalCurrentBalance}/>
+                
+            </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default TotalBalanceBox;
